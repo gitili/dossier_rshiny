d7d20ec76aa6010f1a9d3a23268af3493fd0da47
gitili <ilianabonami@gmail.com>
2025-12-05 16:20:07 +0100

    ajout partie serveur

--- dossier.R
+++ dossier.R
@@ -46,7 +46,43 @@ ui <- page_fluid(
   ) 
 ) 
 
+# SERVEUR ----
 
+server <- function(input, output) {
+  
+  rv <- reactiveValues(df = diamonds, couleur_graph = "Non")
+  
+  observeEvent(input$bouton_graph, {            # Action du bouton "visualiser graph" 
+    rv$df <- diamonds |> 
+      filter(color == input$choix_couleurs) |>  # Filtre avec la couleur choisie dans l'UI
+      filter(price <= input$choix_prix)         # Filtre avec le prix choisi dans l'UI
+    
+    rv$couleur_graph <- input$rose    # Changer la couleur par la couleur choisie dans l'UI
+    
+  })
+  
+  output$distPlot <- renderPlotly({
+    couleur_point <- ifelse(rv$couleur_graph == "Oui", "pink", "black") 
+    graphique <- ggplot(rv$df, aes(x = carat, y = price)) +
+      geom_point(color = couleur_point) +
+      theme_minimal() +
+      labs(title = paste("Prix:", input$choix_prix, "& Couleur:", input$choix_couleurs))
+    ggplotly(graphique)
+  })
+  
+  output$diamonds_tab <- renderDT({
+    # datatable() pour définir les options
+    datatable(rv$df, 
+              options = list(
+                columnDefs = list(
+                  
+                  
+                  list(visible = FALSE, targets = c("x", "y", "z"))
+                )
+              )
+    )
+  })
+}
 
 # Lancer application ----
 shinyApp(ui = ui, server = server)
